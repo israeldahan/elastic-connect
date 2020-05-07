704a41de96d48c590e5ab4e081cc6c7d791b87f2
israel dahan <id@studiodvd.co.il>
2020-05-07 12:51:53 +0300

    init plugin

---
+++ elastic-connect.php
@@ -0,0 +1,17 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+  die( '-1' );
+}
+
+/*
+Plugin Name: Elastic Connect
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: israel dahan
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+
+require_once 'vendor/autoload.php';
